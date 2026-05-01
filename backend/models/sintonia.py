734be10404dcf29e9d0eff75c5a2b20c51b15c14
Christiansalles <christiansallescastilho@gmail.com>
2026-05-01 16:36:38 -0300

    feat: implementa sintonia PID pelo método Cohen-Coon

--- backend/models/sintonia.py
+++ backend/models/sintonia.py
@@ -11,3 +11,15 @@ def sintonizar_chr(k, tau, theta):
         "Ti": round(float(Ti), 4),
         "Td": round(float(Td), 4),
     }
+
+
+def sintonizar_cc(k, tau, theta):
+    r = theta / tau
+    Kp = (1 / k) * (tau / theta) * (4/3 + r/4)
+    Ti = theta * (32 + 6*r) / (13 + 8*r)
+    Td = 4 * theta / (11 + 2*r)
+    return {
+        "Kp": round(float(Kp), 4),
+        "Ti": round(float(Ti), 4),
+        "Td": round(float(Td), 4),
+    }
